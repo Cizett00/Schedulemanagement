0a773ee01f02f2b6da67bcf4e82a572e2c1322a3
Cizett00 <dltkdalsakak@gmail.com>
2026-07-01 13:03:24 +0900

    댓글 개수 제한기능 추가

--- src/main/java/org/example/schedule_managemant/service/CommentService.java
+++ src/main/java/org/example/schedule_managemant/service/CommentService.java
@@ -9,6 +9,8 @@ import org.example.schedule_managemant.repository.ScheduleRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class CommentService {
@@ -21,12 +23,21 @@ public class CommentService {
         Schedule schedule = scheduleRepository.findById(scheduleId)
                 .orElseThrow(() -> new IllegalStateException("없는 일정입니다."));
 
+        List<Comment> commentList = commentRepository.findAll();
+        if(commentList
+                .stream()
+                .filter(c -> c.getSchedule_id().equals(scheduleId))
+                .count() >= 10){
+            throw new IllegalStateException("댓글은 10개 까지 작성할 수 있습니다.");
+        }
+
         Comment comment = new Comment(
                 requestDto.getCm_content(),
                 requestDto.getCm_name(),
                 requestDto.getCm_password(),
                 scheduleId
         );
+
         Comment saveComment = commentRepository.save(comment);
 
         return new CreateCommentResponseDto(
